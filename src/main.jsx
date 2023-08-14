3c1dbf643b726f64d7c0550ad50e4e7f06dabacd
gurmanbenipal <134737006+gurmanbenipal@users.noreply.github.com>
2023-08-14 15:21:54 -0700

    downloaded router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './index.css';
+import App from './pages/App/App.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+      <Router><App /></Router>
+  </React.StrictMode>
+);
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,29 +1,27 @@
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import './App.css'
-import MovieListPage from '../MovieListPage/MovieListPage';
-import MovieDetailPage from '../MovieDetailPage/MovieDetailPage';
+import MovieListPage from '../MoviesListPage/MoviesListPage';
+import MovieDetailPage from '../MoviesDetailPage/MoviesDetailPage';
 import ActorListPage from '../ActorListPage/ActorListPage';
 import LoginPage from '../LoginPage/LoginPage';
 import NavBar from '../../components/NavBar/NavBar';
-
+import './App.css'
 
 export default function App() {
   const [user, setUser] = useState({});
-
   return (
     <main className="App">
       { user ?
-          <>
-            <NavBar />
-            <Routes>
-              <Route path="/" element={<MovieListPage />} />
-              <Route path="/actors" element={<ActorListPage />} />
-              <Route path="/movies/:movieName" element={<MovieDetailPage />} />
-            </Routes>
-          </>
-          :
-          <LoginPage />
+      <>
+        <NavBar />
+        <Routes>
+            <Route path="/" element={<MovieListPage />} />
+            <Route path="/movies/:movieName" element={<MovieDetailPage />} />
+            <Route path="/actors" element={<ActorListPage />} />
+        </Routes>
+      </>
+      :
+      <LoginPage />
       }
     </main>
   );
